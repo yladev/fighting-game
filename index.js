1fb26b5ffe48570be864a35262e6dda5c8880c35
yladev <yanlinnaung881@gmail.com>
2023-11-29 17:43:36 +0630

    set up project

---
+++ index.js
@@ -0,0 +1,48 @@
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = 1024;
+canvas.height = 576;
+
+c.fillRect(0, 0, canvas.width, canvas.height);
+
+class Sprite {
+  constructor({ position, velocity }) {
+    this.position = position;
+    this.velocity = velocity;
+  }
+
+  draw() {
+    c.fillStyle = "red";
+    c.fillRect(this.position.x, this.position.y, 50, 150);
+  }
+
+  update() {
+    this.draw();
+    this.position.y += 10;
+  }
+}
+
+const player = new Sprite({
+  position: {
+    x: 0,
+    y: 0,
+  },
+  velocity: {
+    x: 0,
+    y: 0,
+  },
+});
+
+const enemy = new Sprite({
+  x: 400,
+  y: 100,
+});
+
+console.log(player);
+
+function animate() {
+  window.requestAnimationFrame(animate);
+  console.log("go");
+}
+animate();
